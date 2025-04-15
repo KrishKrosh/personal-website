603ec8f443cf808c0554491e5c489de7562d91f3
KrishKrosh <krish.life21@gmail.com>
2025-04-15 04:21:20 -0400

    loading

--- components/loading-screen.tsx
+++ components/loading-screen.tsx
@@ -1,8 +1,11 @@
 export default function LoadingScreen() {
   return (
-    <div className="h-screen w-full flex items-center justify-center bg-black">
-      <div className="text-gold-500 text-xl">Loading the magic...</div>
+    <div className="h-screen w-full flex items-center justify-center bg-black/80">
+      <div className="ethereal-loader">
+        <div className="orb orb1" />
+        <div className="orb orb2" />
+        <div className="orb orb3" />
+      </div>
     </div>
   )
 }
-
