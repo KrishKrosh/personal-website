8bc51e24b292e0239e7116438a54f20654a05f1f
KrishKrosh <krish.life21@gmail.com>
2025-04-16 05:41:47 -0400

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Krish Shah',
+  description: 'A personal website, a magic trick, and a walk into the most important parts of my life. Pick a card, any card.',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
 }
 
